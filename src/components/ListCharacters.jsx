e5ca9dde639deec457374926fa25d6c78a0f6b15
natlopar <natalialopezariel@gmail.com>
2024-02-10 00:39:43 +0100

    second back

--- src/components/ListCharacters.jsx
+++ src/components/ListCharacters.jsx
@@ -4,23 +4,19 @@ import PropTypes from 'prop-types';
 import '../styles/layout/list.scss';
 
 function ListCharacters({ characters }) {
-
   const charactersSection = characters.map((char) => (
-      <li key={char.id} className='list__card'>
-        <Link to={`/character/${char.id}`} className='link'>
-          
-            <CardCharacter char={char} />
-         
-        </Link>
-      </li>
-    ));
-  
+    <li key={char.id} className="list__card">
+      <Link to={`/character/${char.id}`} className="link">
+        <CardCharacter char={char} />
+      </Link>
+    </li>
+  ));
 
-  return <ul className='list'>{charactersSection}</ul>;
+  return <ul className="list">{charactersSection}</ul>;
 }
 
 ListCharacters.propTypes = {
-  characters: PropTypes.array
-}
+  characters: PropTypes.array,
+};
 
 export default ListCharacters;
