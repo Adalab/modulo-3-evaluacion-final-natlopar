20789b2dd33b0e9ac8c32740c34e6b08d3e817db
natlopar <natalialopezariel@gmail.com>
2024-02-11 21:08:12 +0100

    changes vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nombre-repo/",
+  base: "/modulo-3-evaluacion-final-natlopar/",
 });
